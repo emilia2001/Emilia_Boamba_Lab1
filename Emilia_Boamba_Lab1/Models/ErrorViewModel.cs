257f7b43db0f3d4131f875a9f6153e1afdba25d9
emilia.boamba <Caracatita@2001>
2023-10-19 15:02:39 +0300

    Initial commit

---
+++ Emilia_Boamba_Lab1/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Emilia_Boamba_Lab1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
